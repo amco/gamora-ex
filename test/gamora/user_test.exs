062c4a46ef8f51d6a9a6a441c79ba6fdb2abce7f
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2025-05-14 12:35:17 -0600

    Improve tests

--- test/gamora/user_test.exs
+++ test/gamora/user_test.exs
@@ -7,23 +7,19 @@ defmodule Gamora.UserTest do
     id: 1,
     roles: %{},
     email: "darth@email.com",
+    username: "darthvader",
     first_name: "Darth",
     last_name: "Vader",
     phone_number: "523344556677",
     email_verified: true,
+    associated_user_id: "1234",
     phone_number_verified: true
   }
 
   describe "__struct__/1" do
     test "creates struct with valid attributes" do
-      user = User.__struct__(@attributes)
+      user = struct(User, @attributes)
       assert Map.take(user, Map.keys(@attributes)) == @attributes
-    end
-
-    test "raises error with invalid attributes" do
-      assert_raise KeyError, fn ->
-        User.__struct__(%{invalid: 1})
-      end
     end
   end
 end
